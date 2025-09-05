572f80be0d91208b7b8223435205dcba30387791
Your name <omowunmidaud1@gmail.com>
2025-09-05 11:45:10 +0100

    updated

--- frontend/src/services/index.tsx
+++ frontend/src/services/index.tsx
@@ -20,3 +20,11 @@ export const fetchAllUsersWithPagination = async (options: {
     const data = await response.json();
     return data;
 };
+
+//http://localhost:3001/posts?userId=ee10b0e8346a4a0d990668fd1155fbc2
+//http://localhost:3001/users
+//http://localhost:3001/users/count
+//http://localhost:3001/users/ee10b0e8346a4a0d990668fd1155fbc2/adders
+//get: http://localhost:3001/users/ee10b0e8346a4a0d990668fd1155fbc2/adders
+//delete: http://localhost:3001/posts/a92683a2abde49b98a08114022924e23
+//post: http://localhost:3001/posts
